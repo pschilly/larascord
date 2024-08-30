0dbf14b61dbd77b28507faff6dde97b233aeb165
Schilly <patrick.schillemat@gmail.com>
2024-08-30 17:44:00 -0400

    Remove unwanted items

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -72,37 +72,9 @@ class InstallCommand extends Command
             return;
         }
 
-        // Installing laravel/breeze
-        $this->info('Installing Laravel Breeze...');
-        if ($this->darkMode) {
-            shell_exec('php artisan breeze:install blade --dark');
-        } else {
-            shell_exec('php artisan breeze:install blade');
-        }
-
         // Appending the secrets to the .env file
         $this->info('Appending the secrets to the .env file...');
         $this->appendToEnvFile();
-
-        // Creating the user migration files
-        $this->info('Creating the user migration files...');
-        $this->createUserMigrationFiles();
-
-        // Create the model files
-        $this->info('Creating the user model files...');
-        $this->createModelFiles();
-
-        // Create the view files
-        $this->info('Creating the view files...');
-        $this->createViewFiles();
-
-        // Create the event files
-        $this->info('Creating the event files...');
-        $this->createEventFiles();
-
-        // Remove Laravel Breeze routes
-        $this->info('Replacing the Laravel Breeze routes...');
-        $this->replaceBreezeRoutes();
 
         // Asking the user to build the assets
         if ($this->confirm('Do you want to build the assets?', true)) {
@@ -186,69 +158,5 @@ class InstallCommand extends Command
 
         (new Filesystem())->append('.env', PHP_EOL);
         (new Filesystem())->append('.env', 'LARASCORD_SCOPE=identify&email');
-    }
-
-    /**
-     * Create the user migration files.
-     */
-    public function createUserMigrationFiles(): void
-    {
-        (new Filesystem())->ensureDirectoryExists(database_path('migrations'));
-        (new Filesystem())->copyDirectory(__DIR__ . '/../../database/migrations/', database_path('migrations/'));
-    }
-
-    /**
-     * Create the user model files.
-     */
-    public function createModelFiles(): void
-    {
-        (new Filesystem())->ensureDirectoryExists(app_path('Models'));
-        (new Filesystem())->copy(__DIR__ . '/../../Models/User.php', app_path('Models/User.php'));
-    }
-
-    /**
-     * Create the view files.
-     */
-    public function createViewFiles(): void
-    {
-        (new Filesystem())->ensureDirectoryExists(resource_path('views'));
-        (new Filesystem())->copyDirectory(__DIR__ . '/../../resources/views', resource_path('views'));
-
-        if (!$this->darkMode) {
-            $this->removeDarkClasses((new Finder())
-                    ->in(resource_path('views'))
-                    ->name('*.blade.php')
-                    ->notName('welcome.blade.php')
-            );
-        }
-    }
-
-    /**
-     * Create the event files.
-     */
-    public function createEventFiles(): void
-    {
-        (new Filesystem())->ensureDirectoryExists(app_path('Events'));
-        (new Filesystem())->copyDirectory(__DIR__ . '/../../Events/', app_path('Events/'));
-    }
-
-    /**
-     * Removes Laravel Breeze's default routes and replaces them with Larascord's routes.
-     */
-    public function replaceBreezeRoutes(): void
-    {
-        (new Filesystem())->ensureDirectoryExists(resource_path('routes'));
-        (new Filesystem())->copy(__DIR__ . '/../../routes/web.php', base_path('routes/web.php'));
-        (new Filesystem())->delete(base_path('routes/auth.php'));
-    }
-
-    /**
-     * Remove Tailwind dark classes from the given files.
-     */
-    protected function removeDarkClasses(Finder $finder): void
-    {
-        foreach ($finder as $file) {
-            file_put_contents($file->getPathname(), preg_replace('/\sdark:[^\s"\']+/', '', $file->getContents()));
-        }
     }
 }
